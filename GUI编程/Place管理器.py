90486f2391946af1f9a13150ff7c4a48d14585e6
Git-tzh <1147894846@qq.com>
2021-06-11 11:44:54 +0800

    06.11第一次提交

---
+++ GUI编程/Place管理器.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# 测试使用Place管理器
+
+from tkinter import *
+root = Tk(); root.geometry("500x300")
+root.title("布局管理器place");root["bg"] = "white"
+
+f1 = Frame(root, width=200, height=200, bg="green")
+f1.place(x=30, y=30)
+
+Button(root, text="尚学堂").place(relx=0.2, x=100, y=20, relwidth=0.2, relheight=0.5)
+Button(f1, text="百战程序员").place(relx=0.6, rely=0.7)
+Button(f1, text="唐展豪同学").place(relx=0.5, rely=0.2)
+root.mainloop()
---
+++ GUI编程/扑克游戏界面设计+事件处理.py
@@ -0,0 +1,43 @@
+"""扑克游戏的界面设计"""
+from tkinter import *
+
+
+class Application(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)            # super()代表的是父类的定义而不是父类对象
+        self.master = master
+        self.pack()
+        self.createWidget()
+
+    def createWidget(self):
+        """ 通过place布局管理器实现扑克牌位置控制 """
+        # self.Photo = PhotoImage(file="img/puke/puke1.gif")
+        # self.puke1 = Label(self.master, image=self.photo)
+        # self.puke1.place(x=10, y=50)
+        self.photos = [PhotoImage(file="../imgs/puke/puke"+str(i+1)+".gif") for i in range(10)]
+        self.pukes = [Label(self.master, image=self.photos[i]) for i in range(10)]
+        for i in range(10):
+            self.pukes[i].place(x=10+i*30, y=50)
+
+        # 为所有的Lable增加事件处理
+        self.pukes[0].bind_class("Label", "<Button-1>", self.chupai)
+
+    """ 增加点击事件 """
+    def chupai(self, event):
+        # 打印坐标
+        print(event.widget.winfo_geometry())
+        print(event.widget.winfo_y())
+        # 判断点击事件
+        if event.widget.winfo_y() == 50:
+            event.widget.place(y=30)
+        else:
+            event.widget.place(y=50)
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("扑克牌")
+    root.geometry("450x330+200+300")
+    app = Application(master=root)
+    root.mainloop()
+
