3e0fa6216ef242a7e5ea4fa4c8a57e9255d30d23
Git-tzh <1147894846@qq.com>
2021-12-08 19:43:11 +0800

    12.08京东商品下单补充

--- 功能自动化/TestCase/Test_JD.py
+++ 功能自动化/TestCase/Test_JD.py
@@ -19,25 +19,66 @@ class TestJD(unittest.TestCase):
         driver.maximize_window()
 
         # 加载地址
-        # driver.get("http://t-mintegral437.helitong.cn/#/Login")
-        driver.get("http://t-mintegral437.helitong.cn/#/ShoppingCart")
+        driver.get("http://t-mintegral437.helitong.cn/#/Login")
         # 等待时间
-        sleep(2)
+        sleep(5)
 
-        driver.find_element(By.XPATH, "//div[@role='checkbox']/following::div[5]").click()
-        sleep(2)
 
         """登录"""
         #输入账号
-        # driver.find_element(By.NAME, "mobile").send_keys("13017167459")
-        driver.find_element(By.XPATH, "//input[@placeholder='请输入手机号']").send_keys("13017167459")
+        driver.find_element(By.XPATH, "//input[@name='mobile']/following::input[2]").send_keys("13017167459")
         sleep(1)
         # 输入密码
-        # driver.find_element(By.NAME, "password").send_keys("t123456")
-        driver.find_element(By.XPATH, "//input[@placeholder='请输入账号密码']").send_keys("t123456")
+        driver.find_element(By.NAME, "password").send_keys("t123456")
         sleep(1)
         # 点击登录
-        driver.find_element(By.XPATH, "//div[@class='van-button__text']").click()
+        driver.find_element(By.XPATH, "//button/following::div[10]").click()
+        sleep(3)
+
+        """购物车"""
+        # 点击购物车
+        driver.find_element(By.XPATH, "//div[@class='van-tabbar-item__text']/following::span[2]").click()
+        sleep(1)
+        # 点击第一个勾选框
+        driver.find_element(By.XPATH, "//div[@role='checkbox']/following::div[4]").click()
+        sleep(1)
+        # 结算
+        driver.find_element(By.XPATH, "//div[span=' 全选 ']/following::button").click()
         sleep(2)
-        
-print("------------测试-------------")
+
+        """结算页面"""
+        # 选择使用积分
+        driver.find_element(By.XPATH, "//div[span='去选择']").click()
+        sleep(1)
+
+        # 确认使用积分
+        driver.find_element(By.XPATH, "//span[@class='allintegralcolor']/following::button").click()
+        sleep(1)
+
+        # 提交订单
+        driver.find_element(By.XPATH, "//span[@class='allprice']/following::button").click()
+        sleep(5)
+
+        # 输入密码
+        driver.find_element(By.XPATH, "//input[@class='password-code-input']").send_keys("201518")
+        sleep(1)
+        # driver.find_element(By.XPATH, "//div[@class='password-code-div']//li[2]").send_keys("0")
+        # sleep(1)
+        # driver.find_element(By.XPATH, "//li[@class='password-li'][3]").send_keys("1")
+        # sleep(0.2)
+        # driver.find_element(By.XPATH, "//li[4]").send_keys("5")
+        # sleep(0.2)
+        # driver.find_element(By.XPATH, "//li[5]").send_keys("1")
+        # sleep(0.2)
+        # driver.find_element(By.XPATH, "//li[6]").send_keys("8")
+        # sleep(0.2)
+
+        # 确认
+        driver.find_element(By.XPATH, "//div[@class='buttons-bar']/button[2]").click()
+        sleep(10)
+
+
+
+    # 用例执行完关闭页面
+    def tearDown(self) -> None:
+        driver.close()
